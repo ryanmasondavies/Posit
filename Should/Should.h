a5233b3010a3d0eea790851055fc961a6f052a00
Ryan Davies <ryan@ryandavies.net>
2012-11-21 15:47:26 +0000

    Initial structure.

---
+++ Should/Should.h
@@ -0,0 +1,13 @@
+//
+//  Should.h
+//  Should
+//
+//  Created by Ryan Davies on 21/11/2012.
+//  Copyright (c) 2012 Ryan Davies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Should : NSObject
+
+@end
