4d86b85b9357a64d1964b3af62561b640ca10eba
Ryan Davies <ryan@ryandavies.net>
2013-04-27 20:30:06 +0100

    BeGreaterThan -> Greater

--- Specs/Unit Specs/PSTGreaterMatcherSpec.m
+++ Specs/Unit Specs/PSTGreaterMatcherSpec.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-SpecBegin(PSTBeGreaterThanMatcher)
+SpecBegin(PSTGreaterMatcher)
 
 when(@"comparing to 1", ^{
     it(@"does not match 1", PENDING);
