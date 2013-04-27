2ab01cd4dc4e5b63697fd624a61d460699550979
Ryan Davies <ryan@ryandavies.net>
2013-04-27 20:31:28 +0100

    BeLessThan -> Less

--- Specs/Unit Specs/PSTLessMatcherSpec.m
+++ Specs/Unit Specs/PSTLessMatcherSpec.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-SpecBegin(PSTBeLessThanMatcher)
+SpecBegin(PSTLessMatcher)
 
 when(@"comparing to 1", ^{
     it(@"does match 1", PENDING);
