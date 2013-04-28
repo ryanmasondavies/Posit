9dfea1d588506a7ab5e0d22e160779734994a08c
Ryan Davies <ryan@ryandavies.net>
2013-04-28 09:02:03 +0100

    Implemented less than matcher.

--- Posit/PSTLessMatcher.h
+++ Posit/PSTLessMatcher.h
@@ -1,17 +1,17 @@
 // The MIT License
-// 
+//
 // Copyright (c) 2013 Ryan Davies
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,9 +20,17 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "PSTExpectation.h"
-#import "PSTMatcherInverter.h"
-#import "PSTEqualityMatcher.h"
-#import "PSTIdentityMatcher.h"
-#import "PSTGreaterMatcher.h"
-#import "PSTLessMatcher.h"
+#import <Foundation/Foundation.h>
+#import "PSTMatcher.h"
+
+/** Compares values to a number, returning TRUE if the value is less than the number, and FALSE if not. */
+@interface PSTLessMatcher : NSObject <PSTMatcher>
+
+/**
+ Initialize a less matcher.
+ @param number The number to which values will be compared.
+ @return An initialized matcher.
+ */
+- (id)initWithNumber:(NSNumber *)number;
+
+@end
