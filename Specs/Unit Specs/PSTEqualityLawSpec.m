811276520f313ed48cf3d878a2738aac0a59a5a5
Ryan Davies <ryan@ryandavies.net>
2013-05-20 10:55:53 +0100

    Law, not matcher.

--- Specs/Unit Specs/PSTEqualityLawSpec.m
+++ Specs/Unit Specs/PSTEqualityLawSpec.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-SpecBegin(PSTEqualityMatcher)
+SpecBegin(PSTEqualityLaw)
 
 __block id<PSTLaw> law;
 
